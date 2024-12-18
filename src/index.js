7d10e3153f1b31cf8290a7893a9586f7b61acd05
Leonel Donati <leonelroman321@gmail.com>
2024-12-18 14:56:09 -0300

    added some middlewares

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import express from "express";
 import { PORT } from "./variables.js";
+import { __auth } from "./routes/auth.js";
 
-express().listen(PORT, () => console.log(`Listen on port: ${PORT}`));
+const app = express();
+// DISABLE EXPRESS HEADER
+app.disable("x-powered-by");
+
+// PARSE REQUESTS
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//AUTH Route
+app.use(__auth);
+
+// START SERVER
+app.listen(PORT, () => console.log(`Listen on port: ${PORT}`));
