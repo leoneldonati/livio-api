7dc6d7508252daa2a1494c5bcc37e3b9acda71df
Leonel Donati <leonelroman321@gmail.com>
2024-12-18 14:40:01 -0300

    started router created

---
+++ src/db/index.js
@@ -0,0 +1,26 @@
+import { MongoClient } from "mongodb";
+import { CLUSTER_NAME, DB_NAME, DB_PASS, USER_NAME } from "../variables.js";
+
+const MONGO_URI = `mongodb+srv://${USER_NAME}:${DB_PASS}@cluster0.fo3dmlm.mongodb.net/?w=majority`;
+const client = new MongoClient(MONGO_URI, {
+  appName: CLUSTER_NAME,
+  retryWrites: true,
+});
+
+async function startDb() {
+  try {
+    const connection = await client.connect();
+
+    const db = connection.db(DB_NAME);
+
+    return db;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const db = await startDb();
+
+// COLLECTIONS
+
+export const usersCollection = db.collection("users");
--- src/variables.js
+++ src/variables.js
@@ -1,3 +1,9 @@
 process.loadEnvFile();
 
 export const PORT = process.env.PORT ?? 8080;
+
+// DB INFO
+export const DB_PASS = process.env.DB_PASS;
+export const CLUSTER_NAME = process.env.CLUSTER_NAME;
+export const USER_NAME = process.env.USER_NAME;
+export const DB_NAME = process.env.DB_NAME;
